5b0afe1e931b45f5e3b9044bb90d20fc6f57e539
Lucas <lukas_boo@hotmail.com>
2021-09-02 18:57:43 -0300

    creating data extension

---
+++ MoviesSwiftUI/Extension/DataExtension.swift
@@ -0,0 +1,13 @@
+//
+//  DataExtension.swift
+//  MoviesSwiftUI
+//
+//  Created by Lucas Pedroso on 02/09/21.
+//
+
+import Foundation
+
+class Constants {
+    static let API_KEY = "JBzDusoqkuiMuax3qUcGrQMZPR0Cc1Vs"
+    static let BASE_URL = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=&api-key=" + API_KEY
+}
