4d393c45e950299d6a653ada2e9dd43a1f0c4e8e
Lucas <lukas_boo@hotmail.com>
2021-09-02 19:01:31 -0300

    fixing data extension class

--- MoviesSwiftUI/Extension/DataExtension.swift
+++ MoviesSwiftUI/Extension/DataExtension.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 
-class Constants {
-    static let API_KEY = "JBzDusoqkuiMuax3qUcGrQMZPR0Cc1Vs"
-    static let BASE_URL = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=&api-key=" + API_KEY
+public extension Data {
+    func toModel<T: Decodable>() -> T? {
+        return try? JSONDecoder().decode(T.self, from: self)
+    }
 }
